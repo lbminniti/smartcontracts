2451cd08b015293270d0af941ae4488973a6feb5
lbminniti <leandro.bernal.minniti@gmail.com>
2016-12-13 14:41:53 -0200

    Update SimpleSample.java

--- labs/lab03/SimpleSample/src/main/java/example/SimpleSample.java
+++ labs/lab03/SimpleSample/src/main/java/example/SimpleSample.java
@@ -150,7 +150,7 @@ public class SimpleSample extends ChaincodeBase {
 
 	@Override
 	public String getChaincodeID() {
-		return "lbminniti";
+		return "SimpleSample";
 	}
 
 	public static void main(String[] args) throws Exception {
